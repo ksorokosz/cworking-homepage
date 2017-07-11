dd26db39dc4c411c1271053658340d702cb9886c
Kamil Sorokosz <ksorokosz@wp.pl>
2017-07-11 22:09:33 +0200

    deployment script

--- cworking/wsgi.py
+++ cworking/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cworking.settings")
+path = '/var/www/cworking'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "freelancers.settings")
 
 application = get_wsgi_application()
---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -ue
+# Script deploys django app on Apache Server
+#
+
+current_directory=`dirname $0`
+ip_address=$1; shift
+port=$1; shift
+
+python manage.py collectstatic
+
+find -name "*~" -exec rm -vf {} \;
+find -name "*pyc" -exec rm -vf {} \;
+
+# Rest
+ssh -p $port pi@$ip_address 'sudo rm -Rf /var/www/cworking'
+ssh -p $port pi@$ip_address 'sudo mkdir -p /var/www/cworking'
+
+ssh -p $port pi@$ip_address 'mkdir -p /home/pi/cworking'
+scp -P $port -r $current_directory/cworking $current_directory/config $current_directory/main $current_directory/locale $current_directory/static $current_directory/manage.py pi@$ip_address:/home/pi/cworking/
+ssh -p $port pi@$ip_address 'sudo mv /home/pi/cworking /var/www/'
+ssh -p $port pi@$ip_address 'sudo mv /var/www/cworking/cworking/settings.deploy.py /var/www/cworking/cworking/settings.py'
+ssh -p $port pi@$ip_address "sudo cp /var/www/cworking/config/apache.conf /etc/apache2/sites-enabled/000-default"
+ssh -p $port pi@$ip_address "sudo cp -R /var/www/cworking/static/main /var/www/cworking/static/font-awesome-4.7.0 /var/www/static/"
+ssh -p $port pi@$ip_address 'sudo service apache2 reload'
+ssh -p $port pi@$ip_address 'sudo service apache2 restart'
+ssh -p $port pi@$ip_address 'cd /var/www/cworking && python manage.py migrate'
+
